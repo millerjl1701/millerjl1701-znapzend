91dc57541542c0729003dfbfb95d333d1cbedae5
John Saxton <saxton.john@gmail.com>
2018-09-05 12:50:56 -0500

    code transfer of znapzend import process

--- manifests/import.pp
+++ manifests/import.pp
@@ -10,4 +10,21 @@ define znapzend::import (
 ) {
   include ::znapzend
 
+  $filename = regsubst("${pool}/${filesystem}",'\/','_','G')
+
+  file { "/root/znapzend_configs/${filename}.conf":
+    ensure  => 'present',
+    mode    => '600',
+    owner   => '0',
+    group   => '0',
+    content => template('znapzend/znapzend-template.erb'),
+  }
+  exec { "znapzend_import_${filename}":
+    path =>  ["/usr/bin", "/bin", "/sbin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin", "/opt/znapzend/bin"],
+    command =>  "/bin/cat /root/znapzend_configs/${filename}.conf | znapzendzetup import --write ${pool}/${filesystem}",
+    subscribe =>  [ File["/root/znapzend_configs/${filename}.conf"], Zfs["${pool}/${filesystem}"] ],
+    refreshonly =>  true,
+    onlyif =>  "/bin/test -e /${pool}/${filesystem}",
+    notify =>  Service['znapzend']
+  }
 }
