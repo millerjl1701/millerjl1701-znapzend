9edd1e14483a4163be65630725d3949d5c851318
Sergey Ivanov <seriv@cs.umd.edu>
2022-02-03 11:09:52 -0500

    new systemd puppet module does not need and does not have "daemon-reload"

--- manifests/config.pp
+++ manifests/config.pp
@@ -39,8 +39,6 @@ class znapzend::config {
           }
         }
         '7': {
-          include ::systemd::systemctl::daemon_reload
-
           file { '/etc/systemd/system/znapzend.service':
             ensure  => present,
             owner   => 'root',
@@ -48,7 +46,7 @@ class znapzend::config {
             mode    => '0644',
             content => template($znapzend::service_systemd_template),
             require => File['/etc/default/znapzend'],
-          } ~> Class['systemd::systemctl::daemon_reload']
+          }
         }
         default: {
           fail("${::operatingsystem} ${::operatingsystemmajrelease} not supported")
